026aaf9ae34b05186ad0ecef8cc605e5b5a4214b
AnatoleZho <xiaotaozhou@icloud.com>
2021-12-07 22:45:02 +0800

    创建工程

---
+++ Codable/Codable/CustomView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomView.swift
+//  Codable
+//
+//  Created by EastElsoft on 2018/1/19.
+//  Copyright © 2018年 AnatoleZho. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+    
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+
+    }
+
+}
